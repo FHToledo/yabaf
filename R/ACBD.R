043ac319af5c0a36d7b9ca6c482581c4d221bc28
Fernando H Toledo <f.toledo@cgiar.org>
2026-03-19 11:21:43 -0600

    Refactoring ACBD engine

--- R/ACBD.R
+++ R/ACBD.R
@@ -1,3 +1,14 @@
+##' @title ACBD Example Data Set
+##' @name wheat
+##' @description Augmented complete block design example data set. Reproducible results.
+##'     Check data and outputs.
+##' @format data frame with six columns: row, column, block, treat, role and response
+##' @usage data(wheat)
+##' @rdname wheat
+##' @docType data
+##' @importFrom Rdpack reprompt
+NULL
+
 ##' @title Concrete interface for ACBD designs
 ##' @description Specialized Concrete Definition.
 ##' @name ACBD-class
@@ -5,54 +16,80 @@
 ##' @seealso [Breeder() and SplitPlot()] for the pure virtual and concrete
 ##'     abstract template.
 ##' @field report list or analytical summary.
-##' @import methods lme4 asreml emmeans
+##' @import methods asreml
 ##' @examples
-##' data(stg12025, package = "yabaf")
-##' b <- Breeder(stg12025)
-##' l <- ACBD(b)
-##' l$report$fit$sigma
+##' data(wheat, package = "yabaf")
+##' b <- Breeder(wheat)
+##' a <- ACBD(b)
+##' a$report$fit$sigma
 ##' @export ACBD
 ACBD <- setRefClass(
-  Class = "ACBD",
-  contains = "Breeder",
-  fields = c("report" = "list"),
-  methods = list(
-    initialize = function(d) {
-      if(inherits(d, "Breeder")) {
-        .self$data <- d$data
-        .self$log <- d$log
-        .self$report <- .self$analyze()
-      }
-    },
-    analyze = function() {
-      fit <- lme4::lmer(response ~ 1 + role + (1|block) + (1|treat) + (1|row) + (1|column), data = stg12025)
-      ll     <- as.numeric(logLik(fit))
-      reml <- lme4::REMLcrit(fit)
-      sigma <- sigma(fit)
-      anova <- emmeans::joint_tests(fit, lmer.df = "kenward-roger")
-      vc <- lme4::VarCorr(fit)
-      means <- fixef(fit)["(Intercept)"] + ranef(fit)$treat
-      .self$appendLog(event = "report")
-      .self$report <-  list("fit" = list("LogLik" = ll, "REML" = reml, "sigma" = sigma),
-                            "anova" = anova,
-                            "variance" = as.data.frame(vc),
-                            "means" = means)
-    },
-    asList = function() {
-      return(list(data = .self$data, report = .self$report, log = .self$log))
-    }
-  )
+    Class = "ACBD",
+    contains = "Breeder",
+    fields = c("report" = "list"),
+    methods = list(
+        initialize = function(d) {
+            if(inherits(d, "Breeder")) {
+                .self$data <- d$data
+                .self$log <- d$log
+
+                if (!is.factor(.self$data$row))
+                    .self$data$row <- factor(.self$data$row)
+
+                if (!is.factor(.self$data$column))
+                    .self$data$column <- factor(.self$data$column)
+
+                if (!is.factor(.self$data$role))
+                    .self$data$role <- factor(.self$data$role)
+
+                if (!is.factor(.self$data$block))
+                    .self$data$block <- factor(.self$data$block)
+
+                if (!is.factor(.self$data$treat))
+                    .self$data$treat <- factor(.self$data$treat)
+                
+                .self$report <- .self$analyze()
+            }
+        },
+        analyze = function() {
+            asreml::asreml.options(maxit = 100, workspace = 3E8, pworkspace = 1E8, trace = FALSE)
+            fit <- asreml::asreml(response ~ role + at(role, "check"):treat,
+                                  random = ~ block + at(role, "test"):treat,
+                                  residual = ~ ar1(row):ar1(column),
+                                  data = .self$data)
+            ll <- summary(fit)$loglik
+            reml <- -2 * ll
+            sigma <- summary(fit)$sigma
+            anova <- asreml::wald(fit)
+            vc <- summary(fit)$varcomp
+            checks <- subset(predict(fit, classify = "role:treat", levels = list(role = "check"))$pvals,
+                             status == "Estimable", select = -status)
+            tests <- subset(predict(fit, classify = "role:treat", levels = list(role = "test"))$pvals,
+                            !(treat %in% as.character(checks$treat)) & status == "Estimable", select = -status)
+            .self$appendLog(event = "report")
+            .self$report <-  list("fit" = list("LogLik" = ll, "REML" = reml, "sigma" = sigma),
+                                  "anova" = anova,
+                                  "variance" = as.data.frame(vc),
+                                  "means" = rbind(checks, tests))
+        },
+        asList = function() {
+            return(list(data = .self$data, report = .self$report, log = .self$log))
+        }
+    )
 )
 
 setValidity(
-  Class = "ACBD",
-  method = function(object) {
-    if (all(c("role", "row", "column", "block", "treat", "response") %in% names(object$data))) {
-      TRUE
-    } else {
-      "ACBD must contain role, row, column, block, treat and response data"            
+    Class = "ACBD",
+    method = function(object) {
+        if (all(c("role", "row", "column", "block", "treat", "response") %in% names(object$data))) {
+            TRUE
+        } else {
+            "ACBD must contain role, row, column, block, treat and response data"            
+        }
+        if (all(levels(object$data$role) %in% c("check", "test"))) {
+            TRUE
+        } else {
+            "ACBD must contain check and test as levels of role"
+        }
     }
-  }
 )
-
-
--- tests/testthat/test-ACBD.R
+++ tests/testthat/test-ACBD.R
@@ -1,18 +1,25 @@
 library(testthat)
 
-data(stg12025, package = "yabaf")
-b <- Breeder(stg12025)
+data(wheat, package = "yabaf")
+b <- Breeder(wheat)
 l <- ACBD(b)
 
 test_that("Breeder abstract class test", {
-  expect_true(is(b, "Breeder")) 
+    expect_true(is(b, "Breeder")) 
 })
 
 test_that("ACBD concrete class test", {
-  expect_true(is(l, "ACBD")) 
+    expect_true(is(l, "ACBD")) 
 })
 
-test_that("ACBD concrete class sigma check", {
-  expect_equal(round(l$report$fit$sigma, 4), 0.1493) ## see reference
+test_that("ACBD anova terms", {
+    expect_true(all(c("(Intercept)", "role", "at(role, 'check'):treat", "residual (MS)") %in% rownames(a$report$anova)))
 })
 
+test_that("ACBD variance components", {
+    expect_true(all(c("block", "at(role, 'test'):treat", "row:column!R", "row:column!row!cor", "row:column!column!cor") %in% rownames(a$report$variance)))
+})
+
+test_that("ACBD blues and blups", {
+    expect_true(all(unique(a$report$means$treat) %in% levels(a$data$treat)))
+})
